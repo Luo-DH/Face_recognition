a1e321f9d94bef4331bc9694b9270f6bc4973b7c
Luo-DH <974947011@qq.com>
2021-06-17 21:29:18 +0800

    add: 增加login模块以及添加全局context

---
+++ app/src/main/java/com/luo/facerecognition/FaceApplication.kt
@@ -0,0 +1,28 @@
+package com.luo.facerecognition
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+/**
+ * @author: Luo-DH
+ * @date: 2021/6/17
+ */
+class FaceApplication: Application() {
+    companion object {
+        /*
+         * 可以方便的使用FaceApplication.context获取全局的context
+         * 但是编译器提示context不应该设置成静态的，会造成内存泄漏。
+         * 由于application的context全局唯一，会在app启动时候进行初始化，并且
+         * 整个app生命周期都不会进行回收，因此这里的context不会造成内存泄漏，
+         * 可以加上注解忽略警告
+         */
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
--- app/src/main/java/com/luo/facerecognition/MainActivity.kt
+++ app/src/main/java/com/luo/facerecognition/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.luo.facerecognition
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.luo.login.LoginActivity
 
+/**
+ * 主页面，主要供用户选择进入如下页面
+ *  - 蓝牙连接
+ *  - 识别页面
+ *  - 设置页面
+ */
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 }
